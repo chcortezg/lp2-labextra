f49d84fc3590377b507b069100a370acef8e1b63
chcortezg <chcortezgomes@gmail.com>
2025-09-29 17:49:06 -0300

    classe Entregador implementada

--- labextra/src/labextra/frete/Entregador.java
+++ labextra/src/labextra/frete/Entregador.java
@@ -1,5 +1,14 @@
 package labextra.frete;
 
 public class Entregador {
+	protected String nome;
 
+	public Entregador(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}	
 }
