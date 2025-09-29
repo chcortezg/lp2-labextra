fa251f18f11acb8e39f7bb0e94f22f2097185f23
chcortezg <chcortezgomes@gmail.com>
2025-09-29 18:23:18 -0300

    classe ServicoDeEntrega implementada

--- labextra/src/labextra/frete/ServicoDeEntrega.java
+++ labextra/src/labextra/frete/ServicoDeEntrega.java
@@ -1,5 +1,28 @@
 package labextra.frete;
 
-public class ServicoDeEntrega {
+import java.util.ArrayList;
+import java.util.List;
 
+// classe para representar o serviço de entrega
+public class ServicoDeEntrega {
+	private List<Fretavel> itens;
+	
+	// construtor
+	public ServicoDeEntrega() {
+		this.itens = new ArrayList<>();
+	}
+	
+	// método para adicionar um item (carro, motocicleta, funcionário, autônomo)
+	public void adicionarItem(Fretavel item) {
+		itens.add(item);
+	}
+	
+	// método para calcular o custo total do frete
+	public double calcularCustoTotal(double distanciaEmKm) {
+		double total = 0;
+		for (Fretavel item : itens) {
+			total += item.calcularCustoFrete(distanciaEmKm);
+		}
+		return total;
+	}
 }
