7813197080c8d1525ff699becb7b11fad02652a6
chcortezg <chcortezgomes@gmail.com>
2025-09-29 18:00:27 -0300

    classe Funcionario implementada

--- labextra/src/labextra/frete/Funcionario.java
+++ labextra/src/labextra/frete/Funcionario.java
@@ -1,5 +1,15 @@
 package labextra.frete;
 
-public class Funcionario {
+public class Funcionario extends Entregador implements Fretavel {
+	
+	// construtor
+	public Funcionario(String nome) {
+		super(nome);
+	}
 
+	// sobrecarga de método da interface
+	@Override
+	public double calcularCustoFrete(double distanciaEmKm) {
+		return 5; // custo fixo de uma entrega feita por um funcionário
+	}
 }
