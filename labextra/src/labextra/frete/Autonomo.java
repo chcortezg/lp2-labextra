035b1f0410701a70f499a90131b6b57ca389c2ab
chcortezg <chcortezgomes@gmail.com>
2025-09-29 18:00:12 -0300

    classe Autonomo implementada

--- labextra/src/labextra/frete/Autonomo.java
+++ labextra/src/labextra/frete/Autonomo.java
@@ -1,5 +1,15 @@
 package labextra.frete;
 
-public class Autonomo {
+public class Autonomo extends Entregador implements Fretavel {
+	
+	// construtor
+	public Autonomo(String nome) {
+		super(nome);
+	}
 
+	// sobrecarga de método da interface
+	@Override
+	public double calcularCustoFrete(double distanciaEmKm) {
+		return distanciaEmKm * 0.95; // custo variável de uma entrega feita por um autônomo
+	}
 }
