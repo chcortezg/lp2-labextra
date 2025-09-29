8582fe8baf4f53a2dd50e55fe33650e9e8b15eac
chcortezg <chcortezgomes@gmail.com>
2025-09-29 14:34:36 -0300

    classe abstrata Veiculo implementada

--- labextra/src/labextra/frete/Veiculo.java
+++ labextra/src/labextra/frete/Veiculo.java
@@ -1,5 +1,13 @@
 package labextra.frete;
 
-public class Veiculo {
-
+// classe abstrata para representar qualquer veículo
+public abstract class Veiculo {
+	protected String placa;
+	protected int ano;
+	
+	// construtor
+	public Veiculo(String placa, int ano) {
+		this.placa = placa;
+		this.ano = ano;
+	}
 }
