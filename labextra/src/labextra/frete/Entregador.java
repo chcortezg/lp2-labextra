7d697539086febc450efb52783668fdc8de6b27f
chcortezg <chcortezgomes@gmail.com>
2025-09-29 17:54:25 -0300

    comentários adicionados na classe Entregador

--- labextra/src/labextra/frete/Entregador.java
+++ labextra/src/labextra/frete/Entregador.java
@@ -1,13 +1,16 @@
 package labextra.frete;
 
+// classe para representar um entregador autônomo ou um funcionário
 public class Entregador {
 	protected String nome;
-
+	
+	// construtor
 	public Entregador(String nome) {
 		super();
 		this.nome = nome;
 	}
 
+	// getter
 	public String getNome() {
 		return nome;
 	}	
